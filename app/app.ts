cb42ccc01119b5cc480c6443835bcbca4c36d3dd
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-04-26 16:11:04 -0300

    update class null ignore

--- app/app.ts
+++ app/app.ts
@@ -2,8 +2,12 @@ import { NegotiationController } from "./controllers/negotiation-controller.js";
 
 const controller = new NegotiationController();
 const form = document.querySelector('.form');
+if (form) {
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        controller.adiciona();
+    });
+} else {
+    throw Error("Não foi possivel inicialiazar a aplicação. Verifique se o form existe")
+}
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    controller.adiciona();
-});
